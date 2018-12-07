7a6100f160ecaf94ca772c9a756c05921f8c8ed2
Christopher Caserio <cppc@objex.com>
2018-12-07 03:22:45 -0500

    Remove the service worker (mysterious change that broke my build)

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {BrowserRouter} from "react-router-dom";
 
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
 
 import configureStore from './store/configureStore';
 
@@ -24,4 +24,4 @@ ReactDOM.render(app, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
