1f5e1ededac142b9ed80c0d94fe8aae9c790558a
Christopher Caserio <cppc@objex.com>
2018-12-07 03:19:31 -0500

    Add redux dev tools hook

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,14 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import ReduxThunk from 'redux-thunk';
 
 import rootReducer from './reducers'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore(initialState) {
     return createStore(
         rootReducer,
         initialState,
-        applyMiddleware(ReduxThunk)
+        composeEnhancers(applyMiddleware(ReduxThunk))
     );
 }
