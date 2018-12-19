729d6aa63ba1adcf68067996cb6cce229fd626fc
Christopher Caserio <cppc@objex.com>
2018-12-19 12:55:11 -0500

    Factor out credentials

--- src/axios-auth.js
+++ src/axios-auth.js
@@ -1,23 +1,22 @@
 import axios from 'axios';
+import credentials from './credentials';
 
 const baseAPI = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/';
 const signupPath = 'signupNewUser';
 const loginPath = 'verifyPassword';
-// const keyParam = '?key=';
-// const apiKey = 'AIzaSyCL1XxXoGAyAa4HP3QSdR-lQ0eGqSPanFs';
 
 const makePath = path => (baseAPI + path);
 
 export const signUp = axios.create({
     baseURL: makePath(signupPath),
     params: {
-        key: 'AIzaSyCL1XxXoGAyAa4HP3QSdR-lQ0eGqSPanFs'
+        key: credentials.google.apiKey
     }
 });
 
 export const login = axios.create({
     baseURL: makePath(loginPath),
     params: {
-        key: 'AIzaSyCL1XxXoGAyAa4HP3QSdR-lQ0eGqSPanFs'
+        key: credentials.google.apiKey
     }
 });
